1ccbfb8a3796aa80dc8c4ec521ae91a09fc23972
domi-kunz <83969066+domi-kunz@users.noreply.github.com>
2025-02-06 04:59:15 +0100

    Updates

---
+++ precache-manifest.aedda3ad95622c960bb8d7dbe2e6626d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffff9109d0501cf4f45d0c9484f1148d",
+    "url": "/adi-santiago-schnitzeljagd/index.html"
+  },
+  {
+    "revision": "a3cfdfd4b7fc925534c2",
+    "url": "/adi-santiago-schnitzeljagd/static/css/main.64c85e48.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/adi-santiago-schnitzeljagd/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/adi-santiago-schnitzeljagd/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "a3cfdfd4b7fc925534c2",
+    "url": "/adi-santiago-schnitzeljagd/static/js/main.297cd94b.chunk.js"
+  },
+  {
+    "revision": "824a487c58cf19609631",
+    "url": "/adi-santiago-schnitzeljagd/static/js/runtime~main.becea77d.js"
+  }
+]);
